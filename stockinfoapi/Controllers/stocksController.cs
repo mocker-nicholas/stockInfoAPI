76d6cdd01251d1a9c313564861365897b05a2e46
Nick Orth <nicholas.r.orth@gmail.com>
2022-07-17 10:53:09 -0500

    Add project files.

---
+++ stockinfoapi/Controllers/stocksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace stockinfoapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class stocksController : ControllerBase
+    {
+        public async Task LoadStock(string queries)
+        {
+
+        }
+    }
+}
